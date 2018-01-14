8f73f218619f77b875e9a5ede40c84362d6a548d
ridum <ridum1996@gmail.com>
2018-01-13 21:52:19 -0800

    camera features

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile('/views/test.html', {"root":"./"});
+router.get('/', function (req, res, next) {
+  res.sendFile('/views/test.html', { "root": "./" });
+});
+
+router.get('/camera', function (req, res, next) {
+  res.sendFile('/views/camera.html', { "root": "./" });
 });
 
 module.exports = router;
