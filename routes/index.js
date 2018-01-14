7fc6292a898af2960eaef84f82b25435f08b7620
Jixing Li <724667364@qq.com>
2018-01-14 01:03:12 -0800

    Upgrade from http to https.

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.sendFile('/views/test.html', { "root": "./" });
+  res.sendFile('/views/test.html', { "root": "./.." });
 });
 
 router.get('/camera', function (req, res, next) {
-  res.sendFile('/views/camera.html', { "root": "./" });
+  res.sendFile('/views/camera.html', { "root": "./.."});
 });
 
 module.exports = router;
